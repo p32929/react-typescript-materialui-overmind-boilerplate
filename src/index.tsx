eef524d982e90b0e61818f3493f1423ff6e04a02
p32929 <p32929@yahoo.com>
2021-03-10 18:56:41 +0600

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "overmind-react";
+import {ThemeProvider} from '@material-ui/core';
+import {createOvermind} from 'overmind'
+import {config} from './Overmind/OvermindHelper'
+import {theme} from "./Others/Theme";
+import Counter from "./Components/Counter";
+
+const overmind = createOvermind(config)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider value={overmind}>
+            <ThemeProvider theme={theme}>
+                <Counter name="React TypeScript MaterialUI Overmind Boilerplate"/>
+            </ThemeProvider>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
